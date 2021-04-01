fd42d9dfc6645fb88ed1af23f7afb6d8eaecf16e
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2021-03-31 22:05:52 -0400

    Copy rust-web3's contract sample

--- impl/nft-ptr-lib/src/lib.rs
+++ impl/nft-ptr-lib/src/lib.rs
@@ -1,35 +1,75 @@
 use log::info;
 use web3::api::Web3;
+use web3::contract::Contract;
+use web3::types::{Address, U256};
+
 pub struct NftPtrLib<T: web3::Transport> {
     web3: Web3<T>,
+    account: Address,
 }
 
 impl<T: web3::Transport> NftPtrLib<T> {
+    pub fn new(transport: T) -> NftPtrLib<T> {
+        let web3 = web3::Web3::new(transport);
+        NftPtrLib {
+            web3: web3,
+            account: Address::zero(),
+        }
+    }
     pub async fn initialize(&mut self) {
         self.check_not_prod().await;
+        self.account = self.web3.personal().list_accounts().await.unwrap()[0];
+        info!("Account: {}", self.account);
         self.deploy_token_contract().await;
     }
     async fn check_not_prod(&self) {
         let version = self.web3.net().version().await.unwrap();
         info!("Connected to {} network", version);
-        if version == "main" {
+        if version == "1" {
             panic!("Cowardly refusing to run on mainnet and waste real \"money\"");
         }
     }
-    async fn deploy_token_contract(&mut self) {}
+    async fn deploy_token_contract(&mut self) {
+        // rust-web3/examples/contract.rs
+        // TODO(zhuowei): understand this
+        let my_account = self.account;
+        let bytecode =
+            include_str!("../../../contracts/build/contract_token.code").trim_end_matches("\n");
+        let contract = Contract::deploy(
+            self.web3.eth(),
+            include_bytes!("../../../contracts/build/token.json"),
+        )
+        .unwrap()
+        .confirmations(1)
+        .options(web3::contract::Options::with(|opt| {
+            opt.value = Some(5.into());
+            opt.gas_price = Some(5.into());
+            opt.gas = Some(3_000_000.into());
+        }))
+        .execute(
+            bytecode,
+            (
+                U256::from(1_000_000_u64),
+                "My Token".to_owned(),
+                3u64,
+                "MT".to_owned(),
+            ),
+            my_account,
+        )
+        .await
+        .unwrap();
+    }
 }
 
 pub async fn make_nft_ptr_lib_ipc() -> NftPtrLib<web3::transports::Ipc> {
     // TODO(zhuowei): don't hardcode this
     let transport = web3::transports::Ipc::new("TODOTODO").await.unwrap();
-    let web3 = Web3::new(transport);
-    NftPtrLib { web3: web3 }
+    NftPtrLib::new(transport)
 }
 
 pub fn make_nft_ptr_lib_localhost() -> NftPtrLib<web3::transports::Http> {
     let transport = web3::transports::Http::new("http://127.0.0.1:7545").unwrap();
-    let web3 = web3::Web3::new(transport);
-    NftPtrLib { web3: web3 }
+    NftPtrLib::new(transport)
 }
 
 #[cfg(test)]
