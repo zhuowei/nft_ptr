ec3d741896396acd8f3e32a17ea0367b19a2a851
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2021-04-01 01:34:00 -0400

    Add mintTo method in contract

--- contracts/contracts/NftPtrToken.sol
+++ contracts/contracts/NftPtrToken.sol
@@ -2,9 +2,17 @@
 // https://github.com/OpenZeppelin/openzeppelin-contracts
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract NftPtrToken is ERC721 {
-    constructor() ERC721("MyCollectible", "MCO") {
+contract NftPtrToken is ERC721PresetMinterPauserAutoId {
+    constructor(string memory name_, string memory symbol_, string memory baseTokenURI_) ERC721PresetMinterPauserAutoId(name_, symbol_, baseTokenURI_) {
+    }
+
+    function mintId(address to, uint256 tokenId, string memory tokenURI) public {
+        require(hasRole(MINTER_ROLE, _msgSender()), "NftPtrToken: must have minter role to mint");
+        _mint(to, tokenId);
+        // TODO(zhuowei): URI
+        // _setTokenURI(tokenId, tokenURI);
     }
 }
