d30cfd9244e317d0e478582d08f5461a0c1b682a
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2021-04-08 20:37:58 -0400

    Use serde_json to parse the keystore

--- impl/keystore-loader/src/lib.rs
+++ impl/keystore-loader/src/lib.rs
@@ -3,10 +3,44 @@
 // and nft_ptr is ostensibly a library that others can integrate
 
 use secp256k1::SecretKey;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Keystore {
+    crypto: KeystoreCrypto,
+}
+
+#[derive(Deserialize)]
+struct KeystoreCrypto {
+    ciphertext: String,
+    cipher: String,
+    cipherparams: KeystoreCipherParams,
+    kdf: String,
+    kdfparams: KeystoreKdfParams,
+}
+
+#[derive(Deserialize)]
+struct KeystoreCipherParams {
+    iv: String,
+}
+
+#[derive(Deserialize)]
+struct KeystoreKdfParams {
+    dklen: u32,
+    salt: String,
+    n: u32,
+    r: u32,
+    p: u32,
+}
 
 // Loads an account and its associated SecretKey from a keystore.
 // Warning: this is not secure; do NOT ever load a wallet containing real money using this!!
-pub fn load_keystore_from_string(input: &str, password: &str) -> Result<SecretKey, ()> {
+pub fn load_keystore_from_string(
+    input: &str,
+    password: &str,
+) -> Result<SecretKey, serde_json::Error> {
+    let keystore: Keystore = serde_json::from_str(input)?;
+    println!("{}", keystore.crypto.ciphertext);
     panic!("Unimplemented");
 }
 
